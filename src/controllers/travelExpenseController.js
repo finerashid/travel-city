620b39ee011d62121b1820205a6040b1c6944380
Sohail Sayed <sayedsohail150@gmail.com>
2026-06-17 15:23:04 +0530

    Added populate, pagination and sorting to TravelExpense API

--- src/controllers/travelExpenseController.js
+++ src/controllers/travelExpenseController.js
@@ -1,4 +1,6 @@
 const TravelExpense = require('../models/TravelExpense');
+require('../models/User');
+require('../models/TravelItinerary');
 
 const validateObejectId = require("../middlewares/validateObjectId");
 
@@ -20,9 +22,21 @@ const createTravelExpense = async (req, res) => {                       /// To c
     }
 }   
 
-const getTravelExpenses = async (req, res) => {             
+const getTravelExpenses = async (req, res) => {    
+    
+    const page =Number(req.query.page) || 1;
+    const limit =Number(req.query.limit) || 10;
+    const sort = req.query.sort || "-createdAT";
+
+    const skip =(page - 1)* limit;
+    
     try {
-        const travelExpenses = await TravelExpense.find();
+        const travelExpenses = await TravelExpense.find()
+        .populate("user", "name email")
+        .populate("itinerary", "title budget")
+        .sort(sort)
+        .skip(skip)
+        .limit(limit);
 
         return res.status(200).json({
             success: true,
@@ -41,7 +55,9 @@ const getTravelExpenses = async (req, res) => {
 
 const getTravelExpenseById = async (req, res) => {
     try {
-        const expense = await TravelExpense.findById(req.params.id);
+        const expense = await TravelExpense.findById(req.params.id)
+        .populate("user", "name email")
+        .populate("itinerary", "title budget");
  
         if(!expense) {
             return res.status(404).json({
